5f6472c4e2a09b846383ef2bcbfdab1a64c910c8
Michal Stawinski <michal.stawinski@gmail.com>
2012-02-23 01:55:07 +0100

    Added TrackVector to be used with bare Vector2

---
+++ src/eu/nazgee/game/physics/TrackVector.java
@@ -0,0 +1,19 @@
+package eu.nazgee.game.physics;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class TrackVector extends Track {
+	private Vector2 mVector2follow; 
+
+	public TrackVector(Vector2 pVector2follow) {
+		super(1,0);
+		mVector2follow = pVector2follow;
+	}
+
+	@Override
+	public Vector2 getTrack() {
+		mVec.set(mVector2follow);
+
+		return super.getTrack();
+	}
+}
