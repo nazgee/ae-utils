5c62d89fd69e2518495ce264d4bf37dd4901f821
Michal Stawinski <michal.stawinski@gmail.com>
2012-04-24 23:33:16 +0200

    Added SVG support to TiledTextureRegionFactory helper

--- src/eu/nazgee/game/utils/helpers/TiledTextureRegionFactory.java
+++ src/eu/nazgee/game/utils/helpers/TiledTextureRegionFactory.java
@@ -2,6 +2,7 @@ package eu.nazgee.game.utils.helpers;
 
 import java.io.IOException;
 
+import org.andengine.extension.svg.opengl.texture.atlas.bitmap.SVGBitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
 import org.andengine.opengl.texture.region.ITextureRegion;
@@ -22,6 +23,21 @@ public class TiledTextureRegionFactory {
 	 */
 	static public TiledTextureRegion loadTiles(Context ctx, String pBasePath, String pPath, BuildableBitmapTextureAtlas pAtlas) {
 		return loadTiles(ctx, pBasePath, pPath, pAtlas, 0, 0);
+	}
+
+	/**
+	 * Loads all assets from a given directory, and returns them as TiledTextureRegion
+	 * 
+	 * @param ctx to be used while loading
+	 * @param pBasePath Assets base path of TextureRegion factory
+	 * @param pPath directory containing assets
+	 * @param pAtlas atlas to be used to store assets
+	 * @param w width of svg tile
+	 * @param h height of svg tile
+	 * @return
+	 */
+	static public TiledTextureRegion loadTilesSVG(Context ctx, String pBasePath, String pPath, BuildableBitmapTextureAtlas pAtlas, int w, int h) {
+		return loadTilesSVG(ctx, pBasePath, pPath, pAtlas, 0, 0, w, h);
 	}
 
 	/**
@@ -57,6 +73,53 @@ public class TiledTextureRegionFactory {
 
 				Log.d("loadAssets", "Loading " + file);
 				ITextureRegion tex = BitmapTextureAtlasTextureRegionFactory.createFromAsset(pAtlas, ctx, file);
+				arr[i] = tex;
+				i++;
+
+				if (pLoad>0 && i>=pLoad) {
+					break;
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		return new TiledTextureRegion(pAtlas, arr);
+	}
+
+	/**
+	 * Loads given number of svg assets from a given directory, and returns them as TiledTextureRegion
+	 * 
+	 * @param ctx to be used while loading
+	 * @param pBasePath Assets base path of TextureRegion factory
+	 * @param pPath directory containing assets
+	 * @param pAtlas atlas to be used to store assets
+	 * @param pSkip how many assets should be skipped before loading the first one
+	 * @param pLoad how many assets shall be loaded (value<=0 means all)
+	 * @return
+	 */
+	static public TiledTextureRegion loadTilesSVG(Context ctx, String pBasePath, String pPath, BuildableBitmapTextureAtlas pAtlas, int pSkip, int pLoad, int w, int h) {
+		ITextureRegion arr[] = null;
+
+		try {
+			String[] files = ctx.getAssets().list(pBasePath + pPath);
+			if (pLoad > 0) {
+				arr = new ITextureRegion[Math.min(files.length - pSkip, pLoad)];
+			} else {
+				arr = new ITextureRegion[files.length - pSkip];
+			}
+			int i = 0;
+			for (String file : files) {
+				file = pPath + "/" + file;
+
+				if (pSkip > 0) {
+					pSkip--;
+					Log.d("loadAssets", "Skipping " + file);
+					continue;
+				}
+
+				Log.d("loadAssets", "Loading " + file);
+				ITextureRegion tex = SVGBitmapTextureAtlasTextureRegionFactory.createFromAsset(pAtlas, ctx, file, w, h);
 				arr[i] = tex;
 				i++;
 
