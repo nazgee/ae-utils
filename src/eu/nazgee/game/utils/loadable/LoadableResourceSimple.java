362f841d1d3bfa27cc49c5d6b7bbb4d1f624e30f
Michal Stawinski <michal.stawinski@gmail.com>
2012-06-04 01:11:02 +0200

    some minor renaming

--- src/eu/nazgee/game/utils/loadable/LoadableResourceSimple.java
+++ src/eu/nazgee/game/utils/loadable/LoadableResourceSimple.java
@@ -1,7 +1,7 @@
 package eu.nazgee.game.utils.loadable;
 
 
-public abstract class SimpleLoadableResource extends LoadableResource {
+public abstract class LoadableResourceSimple extends LoadableResource {
 
 	private final LoaderResource mLoader = new LoaderResource(this);
 
